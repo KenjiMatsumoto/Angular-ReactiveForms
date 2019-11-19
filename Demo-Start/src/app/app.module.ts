9fa4f7bcb270533eef8935271c127c416fc70f5a
KenjiMatsumoto <kenjimatsumoto1983@gmail.com>
2019-11-19 16:55:44 +0900

    update appmodule

--- Demo-Start/src/app/app.module.ts
+++ Demo-Start/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
 
 @NgModule({
   declarations: [AppComponent, CustomerComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
